321cc2d26d9a38784c11998db12ea39bbc01beeb
Bui Quoc Khanh <quockhanh.abc@gmail.com>
2021-10-01 20:37:40 +0700

    observer: typescrit

--- observer/typescript/index.ts
+++ observer/typescript/index.ts
@@ -1,35 +1,77 @@
-interface ICakeFactory {
-	bake(): string
+interface Observer {
+	update(event: string): boolean
 }
 
-class VanillaCakeFactory implements ICakeFactory {
-	bake(): string {
-		return "vanilla cake"
+interface Subject {
+	subcribe(observer: Observer, event: string): boolean
+	unsubcribe(observer: Observer, event: string): boolean
+	notify(event: string): boolean
+}
+
+class Youtube implements Subject {
+	protected channels: Array<String> = []
+	protected chanToSub: Map<String, Array<Observer>> = new Map()
+
+	constructor(channels: Array<String>) {
+		this.channels = channels
+		for (let chan of channels) {
+			this.chanToSub.set(chan, [])
+		}
+	}
+
+	notify(channel: string): boolean {
+		const subs = this.chanToSub.get(channel)
+		if (subs) {
+			for (const sub of subs) {
+				sub?.update(channel)
+			}
+		}
+		return true
+	}
+
+	subcribe(user: Observer, channel: string): boolean {
+		const index = this.channels.findIndex((chan) => chan === channel)
+		if (index < 0) {
+			return false
+		}
+		const subs = this.chanToSub.get(channel)
+		subs?.push(user)
+		return true
+	}
+	unsubcribe(user: Observer, channel: string): boolean {
+		const indexChannel = this.channels.findIndex((chan) => chan === channel)
+		if (indexChannel < 0) {
+			return false
+		}
+		const subs = this.chanToSub.get(channel)
+		const indexUser = subs?.findIndex((sub) => sub === user)
+		if (indexUser && indexUser > -1) {
+			subs?.splice(indexUser, 1)
+		}
+		return true
 	}
 }
 
-class VanillaCakeFactoryWrapper implements ICakeFactory {
-	protected factory: ICakeFactory
+class User implements Observer {
+	protected name: string
 
-	constructor(factory: ICakeFactory) {
-		this.factory = factory
+	constructor(name: string) {
+		this.name = name
 	}
-	bake(): string {
-		return this.factory.bake()
-	}
-}
 
-class ChocolateCakeFactory extends VanillaCakeFactoryWrapper {
-	bake(): string {
-		return `Chocolate(${this.factory.bake()})`
+	update(event: string): boolean {
+		console.log(`${this.name} gonna watch new videos on ${event}`)
+		return true
 	}
 }
+const channels = ['discovery', 'national geographic', 'hbo', 'cartoon network']
+const youtube = new Youtube(channels)
 
-class CreamCakeFactory extends VanillaCakeFactoryWrapper {
-	bake(): string {
-		return `Cream(${this.factory.bake()})`
-	}
-}
+const movieHolic = new User('kevin')
+const cartoonHolic = new User('ben')
 
-const factory = new CreamCakeFactory(new ChocolateCakeFactory(new VanillaCakeFactory()))
-console.log(factory.bake())
+youtube.subcribe(movieHolic, channels[2])
+youtube.subcribe(cartoonHolic, channels[3])
+
+youtube.notify(channels[2])
+youtube.notify(channels[3])
